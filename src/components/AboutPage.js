b7769f47c43f04fd0079b88628d18c5b51b2c14d
Abigayle Franklin <Abifranklin@gmail.com>
2019-12-15 20:19:41 -0600

    Initial components in place

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                <h1>About</h1>
+                <p>This app uses React.</p>
+            </>
+        );
+    }
+}
+
+export default AboutPage;
